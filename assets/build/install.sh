88b968f96b330882361360eca433fc16675c12e5
Gilbert Brault <gilbert.brault@hotmail.com>
2018-03-02 13:48:25 +0100

    Update install.sh

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-wget --help
+echo
+echo
+
 if [[ ! -f ${INVOICEPLANE_BUILD_DIR}/v${INVOICEPLANE_VERSION}.zip ]]; then 
   echo "Downloading InvoicePlane ${INVOICEPLANE_VERSION}..."
   wget -nv "https://github.com/InvoicePlane/InvoicePlane/releases/download/v${INVOICEPLANE_VERSION}/v${INVOICEPLANE_VERSION}.zip" -O ${INVOICEPLANE_BUILD_DIR}/v${INVOICEPLANE_VERSION}.zip
